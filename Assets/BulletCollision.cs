06f683cacf646db4bbc5dca324870f730f83a697
Justin <ginka13@gmail.com>
2016-03-26 01:28:37 -0700

    Made bullets go towards mouse

--- Assets/BulletCollision.cs
+++ Assets/BulletCollision.cs
@@ -8,15 +8,28 @@ public class BulletCollision : MonoBehaviour {
 	//Basically, when you left click, shoot in direction of the mouse
 	public float speed;
 	// Use this for initialization
+
+	float mouseX;
+	float mouseY;
+	Vector2 mouseCoord;
+
 	void Start () {
 
 		rb2d = GetComponent<Rigidbody2D> ();
-		rb2d.velocity = new Vector2(speed,speed);
+
+		mouseX = Input.mousePosition.x - Screen.width / 2;
+		mouseY = Input.mousePosition.y - Screen.height / 2;
+		mouseCoord = new Vector2 (mouseX, mouseY);
+		//rb2d.velocity = transform.forward * speed;
+		rb2d.AddForce (mouseCoord * speed);
+		//rb2d.velocity = new Vector2(speed,speed);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//rb2d.velocity = transform.forward * speed;
+		rb2d.AddForce (mouseCoord * speed);
 		
 	}
 }
